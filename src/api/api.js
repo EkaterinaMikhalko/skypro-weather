cf53b8e1c8cafb2b77df1d326ad66b03e7c4e7e4
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-08-21 23:21:30 +0300

    прогноз погоды на 5 дней

--- src/api/api.js
+++ src/api/api.js
@@ -7,8 +7,7 @@ export function useGetWeather() {
 
   async function getWeather() {
     try {
-      // const url = `https://api.openweathermap.org/data/3.0/weather?q=${city}&appid=${apiKey}&units=metric`;
-      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ru&units=metric&cnt=15&appid=${apiKey}`;
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ru&units=metric&cnt=40&appid=${apiKey}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error ${response.status}`);
